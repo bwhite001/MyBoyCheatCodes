a26c18d66770f89b4847963c4c0d278c8294407f
Brandon White <bwhite@infoxchange.org>
2021-11-21 18:08:27 +1000

    added Cheat getter

---
+++ makeCheat.js
@@ -0,0 +1,16 @@
+// Use code on website https://etherealgames.com/gba/a/
+var xml = "<?xml version='1.0' encoding='UTF-8' ?><cheats>"
+start.querySelectorAll("li").forEach(function(item, index) {
+    items = item.innerText.split("\n")
+    if (items.length > 1) {
+        codes = items.slice(1);
+        xml += '<cheat type="cb" name="' + items[0] + '" enabled="false">'
+        for (var i = 0; i < codes.length; i++) {
+            xml += '<code>' + codes[i] + '</code>'
+        }
+        xml += "</cheat>"
+    }
+})
+xml += "</cheats>"
+copy(xml)
+console.log(xml)
