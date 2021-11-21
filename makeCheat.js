9982e5923bbed28cbcca9bde38d21173a8668940
Brandon White <bwhite@infoxchange.org>
2021-11-21 19:08:41 +1000

    Added Codes for Each Unit AW2

--- makeCheat.js
+++ makeCheat.js
@@ -1,5 +1,6 @@
 // Use code on website https://etherealgames.com/gba/a/
 var xml = "<?xml version='1.0' encoding='UTF-8' ?><cheats>"
+var start = document.querySelector("article ol")
 start.querySelectorAll("li").forEach(function(item, index) {
     items = item.innerText.split("\n")
     if (items.length > 1) {
